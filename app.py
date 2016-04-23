1f7fd7ba3d751eba77a3be054a0cc9ccbe3cf9f8
Austin Brown <austinbrown34@gmail.com>
2016-04-23 06:54:11 -0600

    scrapes and returns dict of dicts

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ import requests
 import operator
 import re
 import sys
+sys.setrecursionlimit(10000)
 from library import ScraperManager as sm
 from scrapers import YellowstoneOrion
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from collections import Counter
 from bs4 import BeautifulSoup
 from rq import Queue
@@ -13,8 +14,8 @@ from rq.job import Job
 
 from worker import conn
 
-print(sm)
-print(YellowstoneOrion)
+# print(sm)
+# print(YellowstoneOrion)
 # print(sys.modules)
 # print(sm)
 
@@ -41,10 +42,11 @@ def results_job_key(job_key):
     job = Job.fetch(job_key, connection=conn)
 
     if job.is_finished:
-        return str(job.result), 200
+        print("made it here")
+        master_data = jsonify(job.result)
     else:
-        return "Nay!", 202
-
+        master_data =  "Nay!", 202
+    return render_template('../../results.html', master_data=master_data)
 def scrape(url):
     # Get the url that they entered
     try:
@@ -53,13 +55,46 @@ def scrape(url):
         print("Unable to get URL: {}".format(e))
     # Process the HTML in the requst using BeautifulSoup
     if r:
-        raw = BeautifulSoup(r.text, 'html.parser')
-        tags = raw.find_all('a')
-        print(tags)
-        return tags
+        html = r.content.decode('ascii', 'ignore')
+        raw = BeautifulSoup(html, "lxml")
+        raw = raw.prettify()
+        soup = BeautifulSoup(raw,"lxml")
+        tags = soup.find_all('td')
+        sections = soup.find_all('bgcolor="#F2E3B8"')
 
-# @app.route('/scrapers', methods=['GET'])
-# def scrapers_list():
+        owner_fields = ['Primary Owner','Tax ID','Geo Code','Property Address','Legal Description','Property Type']
+        site_fields = ['Neighborhood Code','Parking type','Utilities','Lot Size','Location','Fronting','Parking Prox','Access','Topography']
+        owner_info = {}
+        site_info = {}
+        master_data_sections = []
+        master_data = {}
+        tagsections = soup.find_all('td', {'bgcolor' : '#F2E3B8'})
+
+        for j,k in enumerate(tagsections):
+            print(k)
+            master_data_sections.append(k)
+        for i,e in enumerate(tags):
+
+            d = str(e)
+
+
+            for field in owner_fields:
+                if field in d:
+                    field_value = str(tags[i+1].text.strip())
+                    field_value = re.sub('[^a-zA-Z0-9-_*. ,]', '', field_value)
+                    owner_info[field] = field_value
+            for field in site_fields:
+                if field in d:
+                    field_value = str(tags[i+1].text.strip())
+                    field_value = re.sub('[^a-zA-Z0-9-_*. ,]', '', field_value)
+                    site_info[field] = field_value
+
+        for sections in master_data_sections:
+            master_data[sections] = eval(sections)
+
+
+        return master_data
+
 
 
 if __name__ == '__main__':
