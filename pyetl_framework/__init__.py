7cb430d103fd8b94d2c6ae552c207b9d3ae10e96
Jonathan Porta <jonathan@jonathanPorta.com>
2016-06-25 23:21:32 -0600

    Allow hostname and port to be set with env vars

--- pyetl_framework/__init__.py
+++ pyetl_framework/__init__.py
@@ -1,3 +1,4 @@
+import os
 from .app import App as App
 from .worker import Worker as Worker
 
@@ -9,7 +10,13 @@ from .lib import Transformer as Transformer
 from .lib import Loader as Loader
 
 def run_flask():
-    App.run()
+    hostname = os.getenv('FLASK_HOSTNAME', '127.0.0.1')
+    port = int(os.getenv('FLASK_PORT', '5000'))
+
+    print('Starting pyetl flask server with hostname: {}, port: {}'.format(hostname, port))
+    print('Change hostname and port by setting env vars: FLASK_HOSTNAME and FLASK_PORT')
+
+    App.run(hostname, port)
 
 def run_worker():
     Worker.run()
--- run_flask.py
+++ run_flask.py
@@ -1,3 +1,10 @@
+import os
 from pyetl_framework import App
 
-App.run()
+hostname = os.getenv('FLASK_HOSTNAME', '127.0.0.1')
+port = int(os.getenv('FLASK_PORT', '5000'))
+
+print('Starting pyetl flask server with hostname: {}, port: {}'.format(hostname, port))
+print('Change hostname and port by setting env vars: FLASK_HOSTNAME and FLASK_PORT')
+
+App.run(hostname, port)
